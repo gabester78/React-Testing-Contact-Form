29ceb5a9e2033ef02d6807e29871d69f6724217b
Gabriel Romero <gabester78@gmail.com>
2020-05-07 12:35:16 -0500

    Form fields tested

--- src/components/CF.js
+++ src/components/CF.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
-import ContactForm from "./components/ContactForm";
-
-test("renders App without crashing", () => {
-  render(<App />);
-});
+import ContactForm from "./ContactForm";
 
 test("test first input field", () => {
   const { getByText } = render(<ContactForm />);
